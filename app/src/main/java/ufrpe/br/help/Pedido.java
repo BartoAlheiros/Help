a924dbc4b1584ef15eb6a9503f06f6541ed00405
Bartô Alheiros <alheirosb@gmail.com>
2018-11-10 20:43:00 -0300

    Funcionando MAPA

---
+++ app/src/main/java/ufrpe/br/help/Pedido.java
@@ -0,0 +1,14 @@
+package ufrpe.br.help;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Pedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido);
+    }
+
+}
